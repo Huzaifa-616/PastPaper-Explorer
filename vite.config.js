6a2fa470980dd4f413169ab85ad224e06e59e733
Huzaifa-616 <huzaifa.bravo@gmail.com>
2026-05-27 12:47:00 +0500

    Remove cloudflare vite plugin to fix build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import { cloudflare } from '@cloudflare/vite-plugin'; // <--- IS THIS LINE HERE?
 
 export default defineConfig({
-  plugins: [react()], // If you see cloudflare() here, delete it!
+  plugins: [react()],
+  // Do not add cloudflare plugin here
 });
